5254d714d482d207a1b895081c50142f4b478f43
lhmet <lhmet@ufsm.br>
2020-10-04 15:46:00 -0300

    leaving values beyond the edges

--- data-raw/prep-data74.R
+++ data-raw/prep-data74.R
@@ -1,27 +1,16 @@
 
 easypackages::libraries(c("fuse.prep", "HEgis", "raster"))
 
-## code to prepare `poly74`
-poly74 <- suppressMessages(HEgis::extract_poly(station = 74))
-
-
-## code to prepare `condem74`
-condem74 <- HEgis::extract_condem(
-  raster("~/Dropbox/datasets/GIS/hydrosheds/sa_con_3s_hydrosheds.grd"),
-  poly74,
-  dis.buf = 0
-)
-
-
 ## code to prepare `precclim74`
 b_prec <- fuse.prep::import_nc(varnc = "prec", dest_dir = "input")
 precclim74 <- fuse.prep::annual_climatology(
   meteo_brick = b_prec,
-  poly_station = prep_poly_posto(poly74, dis.buf = 0.25)
+  poly_station = prep_poly_posto(HEgis::poly74, dis.buf = 0.25)
 )
 #plot(precclim74)
+#! This is acceptable only for annual totals
 precclim74[precclim74 == 0] <- NA
-precclim74 <- raster::mask(precclim74, poly74)
+#precclim74 <- raster::mask(precclim74, poly74)
 #plot(precclim74)
 #plot(sf::st_geometry(poly74), add = TRUE)
 
