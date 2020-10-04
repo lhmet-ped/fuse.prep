b31823aee341932d6c85f740e1d26a71530f781b
lhmet <lhmet@ufsm.br>
2020-10-04 20:03:56 -0300

    added function

--- R/elevation-bands.R
+++ R/elevation-bands.R
@@ -72,8 +72,9 @@ z_bands <- function(z, dz = 100, nbands = NULL){
 #' @param nbands numeric scalar. Default: NULL (use `dz` to build elevation
 #' bands).
 #' @export
-#' @return tibble with fraction of precipitation and elevation covered by
-#' elevation bands. The output columns in this tibble are:
+#' @return a \link[tibble:tibble-package]{tibble} with fraction of precipitation
+#' and elevation covered by elevation bands. The output columns in this tibble
+#' are:
 #' \describe{
 #'  \item{zone}{indice elevation zone}
 #'  \item{inf}{lower limit of elevation band}
@@ -139,8 +140,8 @@ elev_bands <- function(con_dem, meteo_raster = NULL, dz = 100, nbands = NULL) {
 #' Elevation bands NetCDF file
 #'
 #' @param elev_tab data.frame output from \code{\link{elev_bands}}
-#' @param lon scalar numeric
-#' @param lat sclar numeric
+#' @param ccoords a \link[tibble:tibble-package]{tibble} with columns `lon`
+#' and `lat`.
 #' @param file_nc character, path to NetCDF file
 #' @param na scalar numeric, Default: -9999
 #'
@@ -149,28 +150,36 @@ elev_bands <- function(con_dem, meteo_raster = NULL, dz = 100, nbands = NULL) {
 #'
 #' @source \url{https://github.com/naddor/tofu/blob/master/input_output_settings/create_elev_bands_nc.R}
 #' @examples
-#' if(FALSE){
-#'  elevation_tab <- elev_bands(
-#'    con_dem = condem74, meteo_raster = precclim74, dz = 100
-#'  )
-#'  elev_bands_nc(elev_tab = elevation_tab,
-#'                lon = -45.5,
-#'                lat = -23.5,
-#'                file_nc = file.path(tempdir(), "elevation_bands_74.nc")
-#'                na = -9999
-#'                )
+#' if (FALSE) {
+#'   elevation_tab <- elev_bands(
+#'     con_dem = condem74, meteo_raster = precclim74, dz = 100
+#'   )
+#'   elev_bands_nc(
+#'     elev_tab = elevation_tab,
+#'     ccoords = centroids(poly74)
+#'     file_nc = file.path(tempdir(), "elevation_bands_74.nc"),
+#'     na = -9999
+#'   )
 #' }
 elev_bands_nc <- function(elev_tab,
-                          lon,
-                          lat,
+                          ccoords,
                           file_nc = "inst/extdata/elevation_bands_74.nc",
                           na = -9999,
-                          force_v4 = TRUE) {
-  # elev_tab = elev_tab_format
-  req_vars <- c("zone", "mean_elev", "area_frac", "prec_frac")
-  checkmate::assert_subset(req_vars, names(elev_tab))
+                          force_v4 = TRUE,
+                          overwrite = FALSE) {
 
-  elev_tab_format <- dplyr::select(elev_tab_format, dplyr::all_of(req_vars))
+  # elev_tab = elev_tab_format; ccoords = poly_coords
+  var_names <- c("area_frac", "mean_elev", "prec_frac")
+  req_vars <- c("zone", var_names)
+  checkmate::assert_subset(req_vars, names(elev_tab))
+  checkmate::assert_subset(c("lon", "lat"), names(ccoords))
+  checkmate::assert_class(ccoords, "data.frame")
+  checkmate::assert_directory_exists(dirname(file_nc))
+
+  elev_tab_format <- dplyr::select(
+    elev_tab_format,
+    dplyr::all_of(req_vars)
+  )
 
   # define dimensions
   dim_elev_bands <- ncdf4::ncdim_def(
@@ -182,27 +191,45 @@ elev_bands_nc <- function(elev_tab,
   dim_lon <- ncdf4::ncdim_def(
     name = "longitude",
     units = "degreesE",
-    vals = lon
+    vals = ccoords[["lon"]]
   )
 
   dim_lat <- ncdf4::ncdim_def(
     name = "latitude",
     units = "degreesN",
-    vals = lat
+    vals = ccoords[["lat"]]
   )
+
+  dim_list <- list(dim_lon, dim_lat, dim_elev_bands)
+
+  # dim_atts <- tibble::tibble(
+  #   name = c("longitude", "latitude", "elevation_band"),
+  #   units = c("degreesE", "degreesN", "-"),
+  #   vals = list(
+  #     ccoords[["lon"]],
+  #     ccoords[["lat"]],
+  #     sort(elev_tab_format[["zone"]])
+  #   )
+  # ) %>%
+  #   purrr::pmap(., ncdf4::ncdim_def)
+
+  ## names(dim_atts) <- c("dim_lon", "dim_lat", "dim_elev_bands")
+
+  ## all.equal(dim_atts,dim_list)
+
 
   # define variables
   area_frac_nc <- ncdf4::ncvar_def(
     name = "area_frac",
     units = "-",
-    dim = list(dim_lon, dim_lat, dim_elev_bands),
+    dim = dim_list,
     missval = na,
     longname = "Fraction of the catchment covered by each elevation band"
   )
   mean_elev_nc <- ncdf4::ncvar_def(
     name = "mean_elev",
     units = "m asl",
-    dim = list(dim_lon, dim_lat, dim_elev_bands),
+    dim = dim_list,
     missval = na,
     longname = "Mid-point elevation of each elevation band"
   )
@@ -210,26 +237,97 @@ elev_bands_nc <- function(elev_tab,
   prec_frac_nc <- ncdf4::ncvar_def(
     name = "prec_frac",
     units = "-",
-    dim = list(dim_lon, dim_lat, dim_elev_bands),
+    dim = dim_list,
     missval = na,
     longname = "Fraction of catchment precipitation that falls on each elevation band"
   )
 
-  # write variables to file
+
+
+  # long_names <- c(
+  #   "Fraction of the catchment covered by each elevation band",
+  #   "Mid-point elevation of each elevation band",
+  #   "Fraction of catchment precipitation that falls on each elevation band"
+  # )
+  # names(long_names) <- var_names
+  #
+  # vars_atts_l <- tibble::tibble(
+  #   name = names(long_names),
+  #   units = c("-", "m asl", "-"),
+  #   dim = lapply(1:length(name), function(i) dim_atts),
+  #   missval = rep(na, length(name)),
+  #   longname = long_names
+  # ) %>%
+  #   purrr::pmap(ncdf4::ncvar_def)
+
+  ## vars_list <- list(area_frac_nc, mean_elev_nc, prec_frac_nc)
+  ## all.equal(vars_atts, vars_list)
+
+
+ # open nc
   nc_conn <- ncdf4::nc_create(
-    filename = file_nc,
-    vars = list(area_frac_nc, mean_elev_nc, prec_frac_nc),
-    force_v4
+    file_nc,
+    list(area_frac_nc,mean_elev_nc,prec_frac_nc)
   )
+
+  # nc_conn <- ncdf4::nc_create(
+  #   filename = file_nc,
+  #   vars = vars_atts,
+  #   force_v4
+  # )
+
+  # write variables to file
   ncdf4::ncvar_put(nc_conn, area_frac_nc, vals = elev_tab_format$area_frac)
   ncdf4::ncvar_put(nc_conn, mean_elev_nc, vals = elev_tab_format$mean_elev)
   ncdf4::ncvar_put(nc_conn, prec_frac_nc, vals = elev_tab_format$prec_frac)
+
+
+  # lapply(
+  #   var_names,
+  #   function(ivar) {
+  #     # ivar = "prec_frac"
+  #     vars_att_sel <- vars_atts_l[[which(unlist(map(vars_atts_l, "name")) == ivar)]]
+  #     #all.equal(vars_att_sel, prec_frac_nc)
+  #     ncdf4::ncvar_put(
+  #       nc = nc_conn,
+  #       varid = vars_att_sel,
+  #       vals = elev_tab_format[[ivar]]
+  #     )
+  #   }
+  # )
+
   ncdf4::nc_close(nc_conn)
-
   checkmate::assert_file_exists(file_nc)
-
-  return(file_nc)
+  file_nc
 }
 
 
 
+#-------------------------------------------------------------------------------
+#' Centroids
+#' @noRd
+#' @source
+st_centroid_within_poly <- function(poly) {
+  # poly = poly74
+  checkmate::assert_choice("codONS", names(poly))
+  poly <- poly["codONS"]
+  # check if centroid is in polygon
+  ctrd <- suppressWarnings(sf::st_centroid(poly, of_largest_polygon = TRUE))
+  in_poly <- diag(suppressMessages(sf::st_within(ctrd, poly, sparse = FALSE)))
+  # replace geometries that are not within polygon with st_point_on_surface()
+  sf::st_geometry(ctrd[!in_poly, ]) <-
+    suppressWarnings(sf::st_geometry(sf::st_point_on_surface(poly[!in_poly, ])))
+  ctrd
+}
+
+#' Centroid of polygon
+#' @inheritParams spatial_average
+#' @return a \link[tibble:tibble-package]{tibble} with columns `lon` and `lat`.
+#' @export
+centroids <- function(poly_station){
+  cll <- st_centroid_within_poly(poly_station)
+  #plot(st_geometry(poly_station))
+  #plot(cll, add = TRUE)
+  cll <- tibble::as_tibble(sf::st_coordinates(cll))
+  dplyr::rename(cll, "lon" = X, "lat" = Y)
+}
