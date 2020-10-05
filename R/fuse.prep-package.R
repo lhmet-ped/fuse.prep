fd40439ea1f2206eaea00b21aba535a42ea7385e
lhmet <lhmet@ufsm.br>
2020-10-04 23:29:21 -0300

    add doc package

--- R/fuse.prep-package.R
+++ R/fuse.prep-package.R
@@ -1,6 +1,19 @@
-#' @keywords internal
-"_PACKAGE"
-
+#' \pkg{fuse.prep}: Preparation of input data for FUSE
+#'
+#' The goal of \pkg{fuse.prep} is to tidy and generate the input data to run FUSE
+#' for a catchement.
+#'
+#'@section \pkg{fuse.prep} functions:
+#'
+#' \itemize{
+#'    \code{\link{elev_bands_nc}}: a convenience function to
+#'    create the elevation bands NetCDF file.
+#' }
+#'
+#'
+#'@docType package
+#'@name fuse.prep
+#'
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
---
+++ R/meteo_forcing_nc.R
@@ -0,0 +1,4 @@
+#' NetCDF file of Meteorological forcings
+meteo_forcing_nc <- function(){
+ 1 + 1
+}
