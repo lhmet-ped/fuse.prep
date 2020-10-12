b70344676e6760c382c26b018e6cda82d400f3fd
lhmet <lhmet@ufsm.br>
2020-10-12 14:30:37 -0300

    add forcing time series data example for fuse

---
+++ data-raw/prep-forc-data74.R
@@ -0,0 +1,11 @@
+
+# as forçantes foram preparadas no fusepoc.prep
+# e a descrição da sua geração será inclusa em uma vignette
+easypackages::libraries(c("dplyr"))
+
+forcdata74 <- readRDS("~/Dropbox/github/my_reps/lhmet/fusepoc-prep/output/hydrodata-posto-74.RDS")
+forcdata74 <- forcdata74 %>%
+  dplyr::select(date:et0, q_obs = qnat_mm)
+
+use_data(forcdata74)
+
