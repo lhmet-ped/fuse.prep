35a9701e7e70ff08d37b8ddb89320d96f172d554
lhmet <lhmet@ufsm.br>
2020-10-12 13:48:05 -0300

    add function to generates netcdf of forcings

--- R/meteo_forcing_nc.R
+++ R/meteo_forcing_nc.R
@@ -1,4 +1,103 @@
-#' NetCDF file of Meteorological forcings
-meteo_forcing_nc <- function(){
- 1 + 1
+#' Collect arguments
+#' @noRd
+.collect_vars <- function(...){
+  nms_l <-  as.list(substitute(...()))
+  l <- list(...)
+  names(l) <- nms_l
+  l
 }
+
+.check_inputs_meteo_forc <- function(variab_list, ctrd, dts,file){
+
+  all_vars <- c("temp", "pr", "pet", "q_obs")
+  vnames <- names(variab_list)
+  checkmate::assert_subset(vnames, all_vars)
+  identical_lengths <- any(diff(unlist(lapply(variab_list, length))) != 0)
+  checkmate::assert_true(identical_lengths)
+  checkmate::assert_subset(class(dts), c("Date", "POSIXct", "POSIXt"))
+  checkmate::assert_subset(c("lon", "lat"), names(ctrd))
+  checkmate::assert_class(ctrd, "data.frame")
+  checkmate::assert_directory_exists(dirname(file))
+  all_vars
+}
+
+
+
+#' NetCDF file of Meteorological forcings
+meteo_forcing_nc <- function(...,
+                             dates,
+                             ccoords,
+                             file_nc = "inst/extdata/meteo_forcings_74.nc",
+                             na = -9999,
+                             force_v4 = TRUE
+                             ){
+
+  data_list <- .collect_vars(...)
+  var_names <- names(data_list)
+
+  # check inputs
+  all_vars <- .check_input(vnames = var_names, ctrd = ccoords, file = file_nc)
+
+
+  # define dimensions
+  dim_atts_l <- tibble::tibble(
+    name = c("longitude", "latitude", "time"),
+    units = c("degreesE", "degreesN", "days since 1970-01-01"),
+    vals = list(
+      ccoords[["lon"]],
+      ccoords[["lat"]],
+      as.numeric(dates)
+    )
+  ) %>%
+    purrr::pmap(., ncdf4::ncdim_def)
+
+  spatial_mode <- 'Catchment'
+
+  # define variables
+  long_names <- c(
+    paste0(spatial_mode, "-averaged daily temperature"),
+    paste0(spatial_mode, "-averaged daily precipitation"),
+    paste0(spatial_mode, "-averaged daily potential evapotranspiration"),
+    "Daily discharge"
+  )
+  names(long_names) <- all_vars
+
+  var_units <- c("degC", rep("mm/day", 3))
+  names(var_units) <- names(long_names)
+
+  # define variables attributes
+  vars_atts_l <- tibble::tibble(
+    name = var_names,
+    units = var_units[names(var_units) %in% var_names],
+    dim = lapply(1:length(name), function(i) dim_atts_l),
+    missval = rep(na, length(name)),
+    longname = long_names[names(long_names) %in% var_names]
+  ) %>%
+    purrr::pmap(ncdf4::ncvar_def)
+
+  # open nc
+  nc_conn <- ncdf4::nc_create(
+    filename = file_nc,
+    vars = vars_atts_l,
+    force_v4
+  )
+
+  # write variables to file
+  lapply(
+    var_names,
+    function(ivar) {
+      ncdf4::ncvar_put(
+        nc = nc_conn,
+        varid = .select_attr_var(vars_atts_l, ivar),
+        vals = data_list[[ivar]]
+      )
+    }
+  )
+  ncdf4::nc_close(nc_conn)
+  checkmate::assert_file_exists(file_nc)
+  file_nc
+}
+
+# !TESTAR
+
+
