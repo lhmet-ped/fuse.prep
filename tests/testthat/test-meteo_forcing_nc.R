53abd70b453563b43d86104b051b21a7aa5796f4
lhmet <lhmet@ufsm.br>
2020-11-29 23:31:47 -0300

    update tests after changes data_list input to forc_tbl

--- tests/testthat/test-meteo_forcing_nc.R
+++ tests/testthat/test-meteo_forcing_nc.R
@@ -7,10 +7,7 @@ test_that("Produces the correct output.", {
   forcings_nc <- tempfile(fileext = ".nc")
   # exporta dados para netcdf
   meteo_forcing_nc(
-    pr = forcdata74[["pr"]],
-    pet = forcdata74[["pet"]],
-    q_obs = forcdata74[["q_obs"]],
-    dates = forcdata74[["date"]],
+    forc_tbl = forcdata74,
     ccoords = centroids(poly_station = poly74),
     file_nc = forcings_nc
   )
@@ -18,46 +15,38 @@ test_that("Produces the correct output.", {
 })
 
 test_that("Produces the correct errors.", {
+  # wrong names
+  x <- dplyr::rename(forcdata74, "prec" = pr)
   expect_error(
-    meteo_forcing_nc(
-      prec = forcdata74[["pr"]],
-      pet = forcdata74[["pet"]],
-      q_obs = forcdata74[["q_obs"]],
-      dates = forcdata74[["date"]],
+    meteo_forcing_nc(x,
+      ccoords = centroids(poly_station = poly74),
+      file_nc = forcings_nc
+    )
+  )
+  # wrong class
+  x <- dplyr::mutate(forcdata74, date = as.character(date))
+  expect_error(
+    meteo_forcing_nc(x,
       ccoords = centroids(poly_station = poly74),
       file_nc = forcings_nc
     )
   )
 
+  # all missing os
+  x <- dplyr::mutate(forcdata74, temp = NA)
   expect_error(
     meteo_forcing_nc(
-      forcdata74[["pr"]],
-      pet = forcdata74[["pet"]],
-      q_obs = forcdata74[["q_obs"]],
-      dates = forcdata74[["date"]],
-      ccoords = centroids(poly_station = poly74),
+      x,
+      ccoords = poly74,
       file_nc = forcings_nc
     )
   )
 
+ # wrong centroids
   expect_error(
     meteo_forcing_nc(
-      pr = forcdata74[["pr"]],
-      pet = forcdata74[["pet"]],
-      q_obs = forcdata74[["q_obs"]],
-      dates = forcdata74[["date"]],
+      forcdata74,
       ccoords = sf::st_coordinates(poly74),
-      file_nc = forcings_nc
-    )
-  )
- # different lenghts
-  expect_error(
-    meteo_forcing_nc(
-      pr = forcdata74[["pr"]][1:10],
-      pet = forcdata74[["pet"]],
-      q_obs = forcdata74[["q_obs"]],
-      dates = forcdata74[["date"]],
-      ccoords = centroids(poly_station = poly74),
       file_nc = forcings_nc
     )
   )
@@ -65,13 +54,21 @@ test_that("Produces the correct errors.", {
   # file not existent
   expect_error(
     meteo_forcing_nc(
-      pr = forcdata74[["pr"]],
-      pet = forcdata74[["pet"]],
-      q_obs = forcdata74[["q_obs"]],
-      dates = forcdata74[["date"]],
+      forcdata74,
       ccoords = centroids(poly_station = poly74),
       file_nc = "crazy/path"
     )
   )
+
+  # using previous version
+  expect_error(
+  meteo_forcing_nc(
+    pr = forcdata74[["pr"]],
+    pet = forcdata74[["pet"]],
+    q_obs = forcdata74[["q_obs"]],
+    dates = forcdata74[["date"]],
+    ccoords = centroids(poly_station = poly74),
+    file_nc = forcings_nc
+  ))
 
 })
